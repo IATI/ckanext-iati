b7abc683470e3c21d4a2ecf7799edc7168579694
Friedrich Lindenberg <friedrich@pudo.org>
2010-08-31 12:42:20 +0200

    more UI work and rudimentary XML parsing

--- ckanext/iati/__init__.py
+++ ckanext/iati/__init__.py
@@ -2,11 +2,37 @@ import inspect
 import os
 from forms.countries import COUNTRIES
 
+
+# TODO move this to helpers proper
 def country_name(code):
     return dict(COUNTRIES).get(code, code)
-
 import ckan.lib.helpers as h
 h.country_name = country_name 
+
+from ckan.model import Package, Group
+import ckan.model.authz
+from ckan.model.authz import setup_user_roles, Role, Action
+def setup_default_user_roles(domain_object, admins=[]):
+    # sets up roles for visitor, logged-in user and any admins provided
+    # admins is a list of User objects
+    assert isinstance(domain_object, (Package, Group))
+    assert isinstance(admins, list)
+    if type(domain_object) == Package:
+        visitor_roles = [Role.READER]
+        logged_in_roles = [Role.READER]
+    elif type(domain_object) == Group:
+        visitor_roles = [Role.READER]
+        logged_in_roles = [Role.READER]
+    setup_user_roles(domain_object, visitor_roles, logged_in_roles, admins)
+
+ckan.model.authz.setup_default_user_roles = setup_default_user_roles
+
+ckan.model.authz.default_role_actions = [
+    (Role.EDITOR, Action.EDIT),
+    (Role.EDITOR, Action.CREATE),
+    (Role.EDITOR, Action.READ),  
+    (Role.READER, Action.READ),
+    ]
 
 class TemplatingPlugin(object):
     
