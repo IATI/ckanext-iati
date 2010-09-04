a7c1ca616ef13c66bf982449004e314f7941da69
Friedrich Lindenberg <friedrich@pudo.org>
2010-09-04 21:53:08 +0200

    extend and adapt forms

--- ckanext/iati/forms/iati.py
+++ ckanext/iati/forms/iati.py
@@ -6,7 +6,7 @@ from pylons.i18n import _, ungettext, N_, gettext
 
 from ckan.lib.helpers import literal
 import ckan.forms.common as common
-from ckan.forms.common import fa_h, TextExtraField
+from ckan.forms.common import fa_h, TextExtraField, RegExRangeValidatingField
 import ckan.model as model
 import ckan.forms.package as package
 from ckan.lib import field_types
@@ -14,6 +14,45 @@ from ckan.lib import field_types
 from countries import COUNTRIES
 
 __all__ = ['get_iati_fieldset']
+
+class CommaListExtraField(RegExRangeValidatingField):
+    '''A form field for two TextType fields, representing a range,
+    stored in 'extra' fields.'''
+    def get_configured(self):
+        field = self.CommaListField(self.name).with_renderer(self.CommaListRenderer)
+        return RegExRangeValidatingField.get_configured(self, field)
+
+    class CommaListField(formalchemy.Field):
+        def sync(self):
+            if not self.is_readonly():
+                pkg = self.model
+                vals = self._deserialize() or []
+                #vals = [v.strip()for v in vals.split(',') if len(v.strip())]
+                pkg.extras[self.name] = vals
+
+    class CommaListRenderer(formalchemy.fields.FieldRenderer):
+        def _get_value(self):
+            if self.value:
+                return self.value
+            extras = self.field.parent.model.extras
+            return extras.get(self.field.name, [])
+
+        def render(self, **kwargs):
+            values = self._get_value()
+            html = fa_h.text_field(self.name, value=', '.join(values), **kwargs)
+            return html
+
+        def render_readonly(self, **kwargs):
+            val = ', '.join(self._get_value())
+            return field_readonly_renderer(self.field.key, val_str)
+
+        def _serialized_value(self):
+            param_val = self.params.get(self.name, u'')
+            return [v.strip()for v in param_val.split(',') if len(v.strip())]
+            
+        def deserialize(self):
+            return self._serialized_value()
+
 
 class SelectExtraField(TextExtraField):
     '''A form field for text from from a list of options, that is
@@ -79,16 +118,28 @@ def build_package_iati_form(is_admin=False):
     
     #Donor (TODO: Generate from crawler)   
     # Editable List, CSV? 
-    builder.add_field(common.TextExtraField('donors'))
+    builder.add_field(CommaListExtraField('donors'))
+    builder.set_field_text('donors', _('Donors'))
+    
+    builder.add_field(CommaListExtraField('donors_type'))
+    builder.set_field_text('donors_type', _('Donor type'))
+    
+    builder.add_field(CommaListExtraField('donors_country'))
+    builder.set_field_text('donors_country', _('Donor country'))
     
     # TODO: Enforce validation
     countries = [(v, k) for k, v in COUNTRIES]
     builder.add_field(SelectExtraField('country', options=countries))
+    builder.set_field_text('country', _('Recipient country'))
     
     #Verification status: enumeration of statuses (checked, not checked etc)
     # TODO: Enforce validation, can probably only be set by admins
     builder.add_field(common.CheckboxExtraField('verified'))
     builder.set_field_text('verified', _('Verification'))
+    
+    builder.add_field(common.CheckboxExtraField('archive_file'))
+    builder.set_field_text('archive_file', _('Archive'))
+    
     
     #Activity period: (Generate from crawler) 
     builder.add_field(common.DateRangeExtraField('activity_period'))
@@ -98,15 +149,15 @@ def build_package_iati_form(is_admin=False):
     #Resource links: to the actual IATI record
     #Number of activities: (Generate from crawler) 
     builder.add_field(common.TextExtraField('activity_count'))
-    builder.set_field_text('activity_count', _('Number of Activities'))
+    builder.set_field_text('activity_count', _('Num. Activities'))
     
     #Date record updated:
     builder.add_field(common.TextExtraField('record_updated'))
-    builder.set_field_text('record_updated', _('Date record updated'))
+    builder.set_field_text('record_updated', _('Record updated'))
     
     #Date data updated: 
     builder.add_field(common.TextExtraField('data_updated'))
-    builder.set_field_text('data_updated', _('Date data updated'))
+    builder.set_field_text('data_updated', _('Data updated'))
     
     
     #License: Need this field even if it may be a standard license
@@ -116,7 +167,7 @@ def build_package_iati_form(is_admin=False):
     #Department 
     # TODO: Make this a group property instead? 
     builder.add_field(common.TextExtraField('department'))
-    builder.set_field_text('verified', _('Department'))
+    builder.set_field_text('department', _('Department'))
     
     #Contact   
     builder.set_field_text('author', _('Contact'))
@@ -137,17 +188,18 @@ def build_package_iati_form(is_admin=False):
         (_('Basic information'), ['name', 'title', 
                                   'author', 'author_email', 'department',]),
         (_('TEMP: Publishing Entity Info'), ['publisher', 'publisher_type']),
-        (_('Details'), ['donors', 'country', 'activity_period', 
+        (_('Details'), ['country', 'donors', 'donors_type', 'donors_country',
                         'record_updated', 'data_updated',
                         'license_id', 'tags', 'notes']),
         (_('Resources'), ['resources']),
         (_('Verification and Analysis'), [
-                        'activity_count'
+                        'activity_period', 
+                        'activity_count', 'archive_file',
                               ]),
         ])
     if is_admin:
-        field_groups[_('Verification and Analysis')].append('state')
         field_groups[_('Verification and Analysis')].append('verified')
+        field_groups[_('Verification and Analysis')].append('state')
     
     builder.set_displayed_fields(field_groups)
     
