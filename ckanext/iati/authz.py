9e921d68c02122fca29ccb197bae8f7a34b02cf0
Friedrich Lindenberg <friedrich@pudo.org>
2010-09-26 17:32:19 +0200

    basic authz setup

---
+++ ckanext/iati/authz.py
@@ -0,0 +1,24 @@
+import logging
+import ckan.model as model
+
+log = logging.getLogger(__name__)
+
+# Authorization extensions 
+
+def validate_authorization_setup():
+    log.warn("Running monkey-patched authorization setup")
+    
+    for role_action in model.Session(model.RoleAction).filter_by(
+                                                        role=model.Role.READER, 
+                                                        action=model.Action.PACKAGE_CREATE):
+        model.Session.delete(role_action)
+    
+    for role_action in model.Session(model.RoleAction).filter_by(
+                                                        role=model.Role.EDITOR, 
+                                                        action=model.Action.GROUP_CREATE):
+        model.Session.delete(role_action)
+    
+    model.setup_default_user_roles(model.System())
+    
+    
+model.validate_authorization_setup = validate_authorization_setup
