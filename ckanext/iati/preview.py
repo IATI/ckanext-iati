6f6f673866edc43bc238c4de4a0641200a8c7d8a
Friedrich Lindenberg <friedrich@pudo.org>
2010-09-28 15:54:34 +0200

    very basic preview facility

---
+++ ckanext/iati/preview.py
@@ -0,0 +1,54 @@
+import logging
+import os 
+from lxml import etree
+from urllib2 import urlopen
+
+
+PREVIEW_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'xsl', 'activities_file_preview.xsl')
+
+log = logging.getLogger(__name__)
+
+
+def get_preview_transformer():
+    fh = open(PREVIEW_PATH, 'r') 
+    tf = etree.XSLT(etree.parse(fh))
+    fh.close()
+    return tf
+
+    
+def parse_iati_xml(url):
+    if not url or not (url.startswith('http:') or url.startswith('https:') or url.startswith('ftp:')):
+        return None
+    try:
+        urlfh = urlopen(url)
+        return etree.parse(urlfh) 
+    except Exception, e:
+        logging.exception(e)
+        return None
+    finally:
+        urlfh.close()
+
+        
+def generate_preview(url):
+    transform = get_preview_transformer()
+    doc = parse_iati_xml(url) 
+    if not doc: 
+        return None
+    #print etree.tostring(doc).encode('utf-8')
+    return etree.tostring(transform(doc))
+
+
+
+from ckan import model, signals
+
+def on_package(pkg):
+    preview = '<table>'
+    for resource in pkg.resources:
+        pkg_preview = generate_preview(resource.url)
+        if pkg_preview is not None:
+            preview += pkg_preview
+    preview += '</table>'
+    pkg.extras['iati:preview'] = preview
+    
+signals.PACKAGE_NEW.connect(on_package)
+signals.PACKAGE_EDIT.connect(on_package)
