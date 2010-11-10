6f4afd034ff2e511c06bca6269e0c13bc9501f0b
Friedrich Lindenberg <friedrich@pudo.org>
2010-11-10 17:18:48 +0100

    minor facets adaptations

--- ckanext/iati/patch.py
+++ ckanext/iati/patch.py
@@ -3,5 +3,14 @@ from forms.countries import COUNTRIES
 # TODO move this to helpers proper
 def country_name(code):
     return dict(COUNTRIES).get(code, code)
+    
+def group_title(name):
+    from ckan import model
+    group = model.Group.by_name(name) 
+    if group is not None:
+        name = group.title
+    return name
+    
 import ckan.lib.helpers as h
 h.country_name = country_name
+h.group_title = group_title
