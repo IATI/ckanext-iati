b779538808a8f696eff3740505db3b461a58e09d
Adrià Mercader <amercadero@gmail.com>
2011-06-23 12:42:53 +0100

    Point form controllers to the old formalchemy controlers in ckan

--- ckanext/iati/theme.py
+++ ckanext/iati/theme.py
@@ -1,10 +1,18 @@
 import os
 
 from ckan.plugins import implements, SingletonPlugin
-from ckan.plugins import IConfigurer
+from ckan.plugins import IConfigurer,IRoutes
 
 class IatiThemeExtension(SingletonPlugin):
     implements(IConfigurer, inherit=True)
+    implements(IRoutes, inherit=True)
+    def before_map(self, map):
+        map.connect('/package/new', controller='package_formalchemy', action='new')
+        map.connect('/package/edit/{id}', controller='package_formalchemy', action='edit')
+        map.connect('/group/new', controller='group_formalchemy', action='new')
+        map.connect('/group/edit/{id}', controller='group_formalchemy', action='edit')
+
+        return map
 
     def update_config(self, config):
         here = os.path.dirname(__file__)
