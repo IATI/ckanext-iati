fa4af4fc5a4cc51f30f364150881fff69c2e7a76
Adrià Mercader <amercadero@gmail.com>
2011-09-13 11:08:00 +0100

    [new forms] Move converters to ckan core

--- ckanext/iati/controllers/package_iati.py
+++ ckanext/iati/controllers/package_iati.py
@@ -11,6 +11,7 @@ from ckan.lib.navl.validators import (ignore_missing,
                                       ignore,
                                       keep_extras,
                                      )
+from ckan.logic.converters import convert_from_extras, convert_to_extras, date_to_db, date_to_form
 from ckan.lib.navl.dictization_functions import Missing, Invalid
 from ckan.lib.field_types import DateType, DateConvertError
 
@@ -84,36 +85,6 @@ class PackageIatiController(PackageController):
 
         return [{'id':group.id,'name':group.name, 'title':group.title} for group in groups if group.state==model.State.ACTIVE]
 
-
-def convert_to_extras(key, data, errors, context):
-
-    extras = data.get(('extras',), [])
-    if not extras:
-        data[('extras',)] = extras
-
-    extras.append({'key': key[-1], 'value': data[key]})
-
-def convert_from_extras(key, data, errors, context):
-
-    for data_key, data_value in data.iteritems():
-        if (data_key[0] == 'extras'
-            and data_key[-1] == 'key'
-            and data_value == key[-1]):
-            data[key] = data[('extras', data_key[1], 'value')]
-
-def date_to_db(value, context):
-    try:
-        value = DateType.form_to_db(value)
-    except DateConvertError, e:
-        raise Invalid(str(e))
-    return value
-
-def date_to_form(value, context):
-    try:
-        value = DateType.db_to_form(value)
-    except DateConvertError, e:
-        raise Invalid(str(e))
-    return value
 
 def convert_to_comma_list(value, context):
 
