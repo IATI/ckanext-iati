a18883d06cb4f3fc48aa65e78d8fb8e99b6f7077
amercader <amercadero@gmail.com>
2013-03-22 11:27:01 +0000

    Use full use name on email if available

--- ckanext/iati/authz.py
+++ ckanext/iati/authz.py
@@ -149,6 +149,7 @@ def send_activation_notification_email(group):
 
     for user in users:
         if user.email:
-            content = publisher_activation_body_template.format(user_name=user.name,
+            user_name = user.fullname or user.name
+            content = publisher_activation_body_template.format(user_name=user_name,
                     group_title=group.title, group_link=group_link)
             send_email(content, subject, user.email)
