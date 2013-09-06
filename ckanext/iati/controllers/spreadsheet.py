5fb4ac9b16a6e758fbcd3229f90e6d101fe00084
Nigel Babu <nigelbabu@gmail.com>
2013-09-06 06:48:15 +0530

    Extensive changes to spreadsheet controller

--- ckanext/iati/controllers/spreadsheet.py
+++ ckanext/iati/controllers/spreadsheet.py
@@ -3,70 +3,71 @@ import csv
 import StringIO
 
 from ckan import model
-from ckan.lib.base import c, request, response, config, h, redirect, render, abort,  BaseController
-from ckan.lib.helpers import json
-from ckan.authz import Authorizer
-from ckan.logic import get_action, NotFound, ValidationError, NotAuthorized
-from ckan.logic.converters import date_to_db
-from ckan.logic.validators import int_validator
-from ckan.lib.navl.validators import not_empty, ignore_empty, not_missing, ignore_missing
+import ckan.new_authz as authz
+from ckan.lib.base import c
+import ckan.plugins as p
 from ckan.lib.navl.dictization_functions import validate
-from ckanext.iati.authz import get_user_administered_groups
 
 from ckanext.iati.logic.validators import (iati_dataset_name_from_csv,
                                            file_type_validator,
                                            date_from_csv,
                                            yes_no,
-                                           country_code,
-                                           strip)
+                                           country_code)
+from ckanext.iati.logic.converters import strip
 
 log = logging.getLogger(__name__)
 
+_not_empty = p.toolkit.get_validator('not_empty')
+_ignore_empty = p.toolkit.get_validator('ignore_empty')
+_ignore_missing = p.toolkit.get_validator('ignore_missing')
+_int_validator = p.toolkit.get_validator('int_validator')
+
 CSV_MAPPING = [
-        ('registry-publisher-id', 'groups', 'name', [not_empty]),
-        ('registry-file-id', 'package', 'name', [not_empty, iati_dataset_name_from_csv]),
+        ('registry-publisher-id', 'groups', 'name', [_not_empty]),
+        ('registry-file-id', 'package', 'name', [_not_empty, iati_dataset_name_from_csv]),
         ('title', 'package', 'title', []),
         ('contact-email', 'package', 'author_email', []),
-        ('state', 'package', 'state', [ignore_missing]),
+        ('state', 'package', 'state', [_ignore_missing]),
         ('source-url', 'resources', 'url', []),
         ('format', 'resources', 'format', []),
-        ('file-type','extras', 'filetype', [ignore_empty, file_type_validator]),
-        ('recipient-country','extras', 'country', [ignore_empty, country_code]),
-        ('activity-period-start','extras', 'activity_period-from', [ignore_empty, date_from_csv]),
-        ('activity-period-end','extras', 'activity_period-to', [ignore_empty, date_from_csv]),
-        ('last-updated-datetime','extras', 'data_updated', [ignore_empty, date_from_csv]),
-        ('activity-count','extras', 'activity_count', [ignore_empty,int_validator]),
-        ('verification-status','extras', 'verified', [ignore_empty,yes_no]),
+        ('file-type','extras', 'filetype', [_ignore_empty, file_type_validator]),
+        ('recipient-country','extras', 'country', [_ignore_empty, country_code]),
+        ('activity-period-start','extras', 'activity_period-from', [_ignore_empty, date_from_csv]),
+        ('activity-period-end','extras', 'activity_period-to', [_ignore_empty, date_from_csv]),
+        ('last-updated-datetime','extras', 'data_updated', [_ignore_empty, date_from_csv]),
+        ('activity-count','extras', 'activity_count', [_ignore_empty,_int_validator]),
+        ('verification-status','extras', 'verified', [_ignore_empty,yes_no]),
         ('default-language','extras', 'language', []),
         ('secondary-publisher', 'extras', 'secondary_publisher', [strip]),
         ]
 
-class CSVController(BaseController):
+class CSVController(p.toolkit.BaseController):
 
 
     def __before__(self, action, **params):
         super(CSVController,self).__before__(action, **params)
 
         if not c.user:
-            abort(403,'Permission denied')
+            p.toolkit.abort(403,'Permission denied')
 
-        self.is_sysadmin = Authorizer().is_sysadmin(c.user)
+        self.is_sysadmin = authz.is_sysadmin(c.user)
 
-        # Groups of which the logged user is admin
-        self.authz_groups = get_user_administered_groups(c.user)
+        # Orgs of which the logged user is admin
+        context = {'model': model, 'user': c.user or c.author}
+        self.authz_orgs = p.toolkit.get_action('organization_list_for_user')(context, c.user)
 
-    def download(self,publisher=None):
+    def download(self, publisher=None):
 
-        context = {'model':model,'user': c.user or c.author}
+        context = {'model': model, 'user': c.user or c.author}
 
         if publisher and publisher not in ['all','template']:
             try:
-                group = get_action('group_show')(context, {'id':publisher})
-            except NotFound:
-                abort(404, 'Publisher not found')
+                org = p.toolkit.get_action('organization_show')(context, {'id': publisher})
+            except p.toolkit.ObjectNotFound:
+                p.toolkit.abort(404, 'Publisher not found')
 
-            if not group['id'] in self.authz_groups and not self.is_sysadmin:
-                abort(403,'Permission denied for this publisher group')
+            if not org['id'] in self.authz_orgs and not self.is_sysadmin:
+                p.toolkit.abort(403,'Permission denied for this publisher organization')
 
         if self.is_sysadmin:
             if publisher:
@@ -74,46 +75,40 @@ class CSVController(BaseController):
                 output = self.write_csv_file(publisher)
             else:
                 # Show list of all available publishers
-                c.groups = get_action('group_list')(context, {'all_fields':True})
-                return render('csv/index.html')
+                orgs = p.toolkit.get_action('organization_list')(context, {'all_fields': True})
+                return p.toolkit.render('csv/index.html', extra_vars={'orgs': orgs})
         else:
             if publisher and publisher != 'all':
                 # Return CSV for provided publisher (we already checked the permissions)
                 output = self.write_csv_file(publisher)
             elif len(self.authz_groups) == 1:
                 # Return directly CSV for publisher
-                output = self.write_csv_file(self.authz_groups[0])
-            elif len(self.authz_groups) > 1:
+                output = self.write_csv_file(self.authz_orgs[0])
+            elif len(self.authz_orgs) > 1:
                 # Show list of available publishers for this user
-                groups = get_action('group_list')(context, {'all_fields':True})
-                c.groups = []
-                for group in groups:
-                    if group['id'] in self.authz_groups:
-                        c.groups.append(group)
-
-                return render('csv/index.html')
+                return p.toolkit.render('csv/index.html', extra_vars={'orgs', self.authz_orgs})
             else:
                 # User does not have permissions on any publisher
-                abort(403,'Permission denied')
+                p.toolkit.abort(403, 'Permission denied')
 
 
         file_name = publisher if publisher else 'iati-registry-records'
-        response.headers['Content-type'] = 'text/csv'
-        response.headers['Content-disposition'] = 'attachment;filename=%s.csv' % str(file_name)
+        p.toolkit.response.headers['Content-type'] = 'text/csv'
+        p.toolkit.response.headers['Content-disposition'] = 'attachment;filename=%s.csv' % str(file_name)
         return output
 
     def upload(self):
         if not self.is_sysadmin and not self.authz_groups:
             # User does not have permissions on any publisher
-            abort(403,'Permission denied')
+            p.toolkit.abort(403,'Permission denied')
 
-        if request.method == 'GET':
-            return render('csv/upload.html')
-        elif request.method == 'POST':
-            csv_file = request.POST['file']
+        if p.toolkit.request.method == 'GET':
+            return p.toolkit.render('csv/upload.html')
+        elif p.toolkit.request.method == 'POST':
+            csv_file = p.toolkit.request.POST['file']
 
             if not hasattr(csv_file,'filename'):
-                abort(400,'No CSV file provided')
+                p.toolkit.abort(400,'No CSV file provided')
 
             c.file_name = csv_file.filename
 
@@ -127,21 +122,21 @@ class CSVController(BaseController):
             log.info('CSV import finished: file %s, %i added, %i updated, %i warnings, %i errors' % \
                     (c.file_name,len(c.added),len(c.updated),len(c.warnings),len(c.errors)))
 
-            return render('csv/result.html')
+            return p.toolkit.render('csv/result.html')
 
-    def write_csv_file(self,publisher):
-        context = {'model':model,'user': c.user or c.author}
+    def write_csv_file(self, publisher):
+        context = {'model': model, 'user': c.user or c.author}
         try:
             if publisher == 'all':
-                packages = get_action('package_list')(context, {})
+                packages = p.toolkit.get_action('package_list')(context, {})
             elif publisher == 'template':
                 # Just return an empty CSV file with just the headers
                 packages = []
             else:
-                group = get_action('group_show')(context, {'id':publisher})
-                packages = [pkg['id'] for pkg in group['packages']]
-        except NotFound:
-            abort(404, 'Group not found')
+                org = p.toolkit.get_action('organization_show')(context, {'id': publisher})
+                packages = [pkg['name'] for pkg in org['packages']]
+        except p.toolkit.ObjectNotFound:
+            p.toolkit.abort(404, 'Group not found')
 
         f = StringIO.StringIO()
 
@@ -155,9 +150,9 @@ class CSVController(BaseController):
             packages.sort()
             for pkg in packages:
                 try:
-                    package = get_action('package_show_rest')(context,{'id':pkg})
+                    package = p.toolkit.get_action('package_show')(context, {'id': pkg})
                     package.pop('state', None)
-                except NotAuthorized:
+                except p.toolkit.NotAuthorized:
                     log.warn('User %s not authorized to read package %s' % (c.user, pkg))
                     continue
                 if package:
@@ -247,17 +242,17 @@ class CSVController(BaseController):
                 self.create_or_update_package(package_dict,counts,context=context)
 
                 del errors[row_index]
-            except ValidationError,e:
+            except p.toolkit.ValidationError,e:
                 iati_keys = dict([(f[2],f[0]) for f in CSV_MAPPING])
                 for key, msgs in e.error_dict.iteritems():
                     iati_key = iati_keys[key]
                     log.error('Error in row %i: %s: %s' % (i+1,iati_key,str(msgs)))
                     errors[row_index][iati_key] = msgs
-            except NotAuthorized,e:
+            except p.toolkit.NotAuthorized,e:
                 msg = 'Not authorized to publish to this group: %s' % row['registry-publisher-id']
                 log.error('Error in row %i: %s' % (i+1,msg))
                 errors[row_index]['registry-publisher-id'] = [msg]
-            except NotFound,e:
+            except p.toolkit.ObjectNotFound,e:
                 msg = 'Publisher not found: %s' % row['registry-publisher-id']
                 log.error('Error in row %i: %s' % (i+1,msg))
                 errors[row_index]['registry-publisher-id'] = [msg]
@@ -302,7 +297,7 @@ class CSVController(BaseController):
 
         package_dict['title'] = package_dict['title'].decode('utf-8')
         try:
-            existing_package_dict = get_action('package_show')(context, data_dict)
+            existing_package_dict = p.toolkit.get_action('package_show')(context, data_dict)
 
             # Update package
             log.info('Package with name "%s" exists and will be updated' % package_dict['name'])
@@ -312,17 +307,17 @@ class CSVController(BaseController):
 
             context['message'] = 'CSV import: update dataset %s' % package_dict['name']
 
-            updated_package = get_action('package_update_rest')(context, package_dict)
+            updated_package = p.toolkit.get_action('package_update_rest')(context, package_dict)
             if counts:
                 counts['updated'].append(updated_package['name'])
             log.debug('Package with name "%s" updated' % package_dict['name'])
-        except NotFound:
+        except p.toolkit.ObjectNotFound:
             # Package needs to be created
             log.info('Package with name "%s" does not exist and will be created' % package_dict['name'])
 
             context['message'] = 'CSV import: create dataset %s' % package_dict['name']
 
-            new_package = get_action('package_create_rest')(context, package_dict)
+            new_package = p.toolkit.get_action('package_create_rest')(context, package_dict)
             if counts:
                 counts['added'].append(new_package['name'])
             log.debug('Package with name "%s" created' % package_dict['name'])
