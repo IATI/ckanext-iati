5613a8a5e33156fa29bce098b951f41c4c3ce1c7
John Martin <me@johnmart.in>
2013-09-10 12:28:32 +0100

    Whitespace fix

--- ckanext/iati/controllers/static.py
+++ ckanext/iati/controllers/static.py
@@ -2,14 +2,14 @@ from ckan.lib.base import render, BaseController
 
 class StaticController(BaseController):
 
-	def using_iati_data(self):
-		return render('static/using-iati-data.html')
+    def using_iati_data(self):
+        return render('static/using-iati-data.html')
 
-	def about(self):
-		return render('static/about-2.html')
+    def about(self):
+        return render('static/about-2.html')
 
-	def api(self):
-		return render('static/registry-api.html')
+    def api(self):
+        return render('static/registry-api.html')
 
-	def help(self):
-		return render('static/help.html')
+    def help(self):
+        return render('static/help.html')
