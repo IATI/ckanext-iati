2067434fb614b62d85898549c7816f30bcb408e1
amercader <amercadero@gmail.com>
2013-09-11 15:37:20 +0100

    Add script for cleaning up the db

---
+++ scripts/cleanup_db.sql
@@ -0,0 +1,13 @@
+DELETE FROM package_extra_revision WHERE key = 'publishertype';
+DELETE FROM package_extra_revision WHERE key = 'publisher_organization_type';
+DELETE FROM package_extra_revision WHERE key = 'publisher_country';
+DELETE FROM package_extra_revision WHERE key = 'publisher_iati_id';
+
+DELETE FROM package_extra WHERE key = 'publishertype';
+DELETE FROM package_extra WHERE key = 'publisher_organization_type';
+DELETE FROM package_extra WHERE key = 'publisher_country';
+DELETE FROM package_extra WHERE key = 'publisher_iati_id';
+
+DELETE FROM package_extra_revision WHERE key LIKE 'iati:preview%'; 
+DELETE FROM package_extra WHERE key LIKE 'iati:preview%'; 
+
