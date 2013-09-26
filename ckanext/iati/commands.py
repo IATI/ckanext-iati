ab1b327bbecab936b3a6034ab38ff0615976a55d
Nigel Babu <nigelbabu@gmail.com>
2013-09-26 08:53:38 +0530

    Create logger after _load_config

--- ckanext/iati/commands.py
+++ ckanext/iati/commands.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import logging
 from ckan.lib.cli import CkanCommand
 
 from ckanext.iati.archiver import run as run_archiver
@@ -39,6 +40,7 @@ class Archiver(CkanCommand):
 
         cmd = self.args[0]
         self._load_config()
+        self.logger = logging.getLogger(__name__)
 
         if cmd == 'update':
             package_id = unicode(self.args[1]) if len(self.args) > 1 else None
