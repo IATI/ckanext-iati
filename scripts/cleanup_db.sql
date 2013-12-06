0f2893a8d400bbf92e53f9835eef934ed0d5a706
amercader <amercadero@gmail.com>
2013-12-06 17:20:09 +0000

    Include 'publisher_source_type' extras in db clean-up

--- scripts/cleanup_db.sql
+++ scripts/cleanup_db.sql
@@ -1,11 +1,13 @@
 BEGIN;
 
 DELETE FROM package_extra_revision WHERE key = 'publishertype';
+DELETE FROM package_extra_revision WHERE key = 'publisher_source_type';
 DELETE FROM package_extra_revision WHERE key = 'publisher_organization_type';
 DELETE FROM package_extra_revision WHERE key = 'publisher_country';
 DELETE FROM package_extra_revision WHERE key = 'publisher_iati_id';
 
 DELETE FROM package_extra WHERE key = 'publishertype';
+DELETE FROM package_extra WHERE key = 'publisher_source_type';
 DELETE FROM package_extra WHERE key = 'publisher_organization_type';
 DELETE FROM package_extra WHERE key = 'publisher_country';
 DELETE FROM package_extra WHERE key = 'publisher_iati_id';
