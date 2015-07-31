9a755d181ced99d004dbed7a8fbc617f6839d15e
Brook Elgie <specialbrew@gmail.com>
2015-07-31 10:14:02 +0100

    Add tmp to fix bower install error

--- ckanext/iati/theme/setup.sh
+++ ckanext/iati/theme/setup.sh
@@ -6,6 +6,8 @@
 
 # Installs less and nodewatch for the compiling of iati.css
 npm install less@1.4.1 watchr@2.4.3
+# tmp is required by bower to prevent bower install error
+npm install tmp@0.0.23
 # Gets the dependency manager bower
 npm install bower@1.3.6
 # Then installs bootstrap
