ac23df009c77e122f074ea755914ba658acfe4ab
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2016-11-01 17:09:16 +0100

    Fix validation error on update

--- ckanext/iati/archiver.py
+++ ckanext/iati/archiver.py
@@ -284,6 +284,11 @@ def update_package(context, data_dict, message=None):
                '{0}'.format(data_dict['name']))
     context['message'] = message
 
+    for extra in data_dict['extras']:
+        data_dict[extra['key']] = extra['value']
+
+    data_dict['extras'] = []
+
     updated_package = toolkit.get_action('package_update')(context, data_dict)
     log.debug('Package {0} updated with new extras'.format(data_dict['name']))
 
