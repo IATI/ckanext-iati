adee398d03fb6e738e08e5868a433934e102d2ad
kmbn <github@nevermindtheumlauts.com>
2017-07-24 11:09:57 +0200

    Return max. no. of datasets when viewing users

--- ckanext/iati/plugins.py
+++ ckanext/iati/plugins.py
@@ -486,6 +486,7 @@ class IatiDatasets(p.SingletonPlugin, p.toolkit.DefaultDatasetForm):
             'group_show',
             'organization_list_publisher_page',
             'organization_list_pending',
+            'user_show',
         )
         return _get_module_functions(iati_actions, function_names)
 
