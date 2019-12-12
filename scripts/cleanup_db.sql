b325f79917f09d3e83c2c4f27062e5a76b9390a7
gtkChop <swaroopbhatk@gmail.com>
2019-12-12 10:31:58 +0000

    bin and scripts to python 3

---
+++ scripts/cleanup_db.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+DELETE FROM package_extra_revision WHERE key = 'publishertype';
+DELETE FROM package_extra_revision WHERE key = 'publisher_source_type';
+DELETE FROM package_extra_revision WHERE key = 'publisher_organization_type';
+DELETE FROM package_extra_revision WHERE key = 'publisher_country';
+DELETE FROM package_extra_revision WHERE key = 'publisher_iati_id';
+
+DELETE FROM package_extra WHERE key = 'publishertype';
+DELETE FROM package_extra WHERE key = 'publisher_source_type';
+DELETE FROM package_extra WHERE key = 'publisher_organization_type';
+DELETE FROM package_extra WHERE key = 'publisher_country';
+DELETE FROM package_extra WHERE key = 'publisher_iati_id';
+
+DELETE FROM package_extra_revision WHERE key LIKE 'iati:preview%'; 
+DELETE FROM package_extra WHERE key LIKE 'iati:preview%'; 
+
+UPDATE resource_revision SET format = 'iati-xml';
+UPDATE resource SET format = 'iati-xml';
+
+COMMIT;
+
