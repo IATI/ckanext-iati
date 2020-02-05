8cf50349d528a65fb6d38fb4c3d8235408a645dd
gtkChop <swaroopbhatk@gmail.com>
2020-02-05 12:42:42 +0000

    Issue Not #234 existing user validation

---
+++ ckanext/iati/logic/user_action.py
@@ -0,0 +1,15 @@
+from ckan.plugins.toolkit import get_action
+from ckanext.iati import helpers as h
+
+
+
+def user_list(context, data_dict):
+    """
+    Functionality: User must be bale to login through email id or username
+    Wrap the core user list by considering the option for user email
+    """
+    val = data_dict['id']
+    if h.email_validator(val):
+        return h.get_user_list_by_email(val)
+
+    return toolkit.get_action(u'user_list')(context, data_dict)
