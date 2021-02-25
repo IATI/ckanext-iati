f36f6bb28dd504bdfe80f1db704f1271258e5c91
gtkChop <swaroopbhatk@gmail.com>
2021-02-25 19:12:55 +0000

    code formating, removed print statements - makes easier to read

--- ckanext/iati/archiver.py
+++ ckanext/iati/archiver.py
@@ -41,9 +41,8 @@ def text_traceback():
         ).strip()
     return res
 
+
 def run(package_id=None, publisher_id=None, pub_id=None):
-
-
     # TODO: use this when it gets to default ckan
     # username = toolkit.get_action('get_site_user')({'model': model,
     #                                                 'ignore_auth': True}, {})
@@ -62,20 +61,15 @@ def run(package_id=None, publisher_id=None, pub_id=None):
     if not context['site_url']:
         raise Exception('You have to set the "ckan.site_url" property in the '
                         'config file')
-        return False
     if not context['user']:
         raise Exception('You have to set the "iati.admin_user.name" property '
                         'in the config file')
-        return False
     if not context['apikey']:
         raise Exception('You have to set the "iati.admin_user.api_key" '
                         'property in the config file')
-        return False
-
 
     # if we are updating a publisher
     publisher_update = False
-
 
     if package_id:
         package_ids = [package_id]
@@ -99,7 +93,6 @@ def run(package_id=None, publisher_id=None, pub_id=None):
     else:
         try:
             package_ids = toolkit.get_action('package_list')(context, {})
-            print "=================== TOTAL PACKAGES FOUND: {} =========================".format(str(len(package_ids)))
         except toolkit.ObjectNotFound:
             res = {}
             log.error('Could not find package: {0}'.format(package_id))
@@ -109,24 +102,17 @@ def run(package_id=None, publisher_id=None, pub_id=None):
             results.append(res)
             return results
 
-
-
     t1 = datetime.datetime.now()
-
-
     log.info('IATI Archiver: starting {0}'.format(str(t1)))
     log.info('Number of datasets to archive: {0}'.format(len(package_ids)))
 
-
     updated = 0
     consecutive_errors = 0
-
 
     for cnt, package_id in enumerate(package_ids, 1):
         result = {}
         updated_package = False
         try:
-            print "========= checking package ========" + package_id + ": " + str(cnt)
             updated_package, issue_type, issue_message = archive_package(package_id, context, consecutive_errors)
             result['publisher_id'] = pub_id
             result['package_id'] = package_id
@@ -144,7 +130,6 @@ def run(package_id=None, publisher_id=None, pub_id=None):
             result['package_id'] = package_id
             result['issue_message'] = 'Error downloading resource for dataset {0}: {1}'.format(package_id, str(e))
             result['issue_type'] = 'Download Error'
-
 
             if consecutive_errors > 15:
                 log.error('Too many errors...')
@@ -170,20 +155,13 @@ def run(package_id=None, publisher_id=None, pub_id=None):
     return results
 
 
-
-
 def archive_package(package_id, context, consecutive_errors=0):
-
 
     from ckanext.archiver import tasks
     package = toolkit.get_action('package_show')(context, {'id': package_id})
     extras_dict = extras_to_dict(package)
-
-
     is_activity_package = (True if 'activity' == extras_dict.get('filetype')
                            else False)
-
-
     log.info('Archiving dataset: {0} ({1} resources)'.format(
               package.get('name'), len(package.get('resources', []))))
     for resource in package.get('resources', []):
@@ -211,11 +189,7 @@ def archive_package(package_id, context, consecutive_errors=0):
         except socket.timeout:
             return save_package_issue(context, package, extras_dict,
                                       'download-error', 'URL timeout')
-
-
         file_path = result['saved_file']
-
-
         if 'zip' in result['headers'].get('content-type', ''):
             # Skip zipped files for now
             log.info('Skipping zipped file for dataset '
@@ -223,24 +197,18 @@ def archive_package(package_id, context, consecutive_errors=0):
             os.remove(file_path)
             continue
 
-
         update = False
-
-
         if old_hash != result.get('hash'):
             update = True
-
 
         with open(file_path, 'r') as f:
             xml = f.read()
         os.remove(file_path)
 
-
         if (re.sub('<!doctype(.*)>', '',
                    xml.lower()[:100]).strip().startswith('<html')):
             return save_package_issue(context, package, extras_dict,
                                       'xml-error', 'File is an HTML document')
-
 
         try:
             tree = etree.fromstring(xml, parser=etree.XMLParser(huge_tree=True))
@@ -248,7 +216,6 @@ def archive_package(package_id, context, consecutive_errors=0):
             return save_package_issue(context, package, extras_dict,
                                       'xml-error', 'Could not parse XML file:'
                                       ' {0}'.format(str(e)[:200]))
-
 
         filetype = 'unchecked'
         if tree.tag == 'iati-activities':
@@ -268,35 +235,26 @@ def archive_package(package_id, context, consecutive_errors=0):
         new_extras = {}
         # IATI standard version (iati_version extra)
         xpath = '/iati-activities/@version | /iati-organisations/@version'
-
-
         version = tree.xpath(xpath)
         log.info(version)
         log.info(version[0])
-
-
         allowed_versions = IATI_STANDARD_VERSIONS
-
-
         if len(version) and version[0] in allowed_versions:
             version = version[0]
         else:
             version = 'n/a'
         new_extras['iati_version'] = version
 
-
         if is_activity_package:
             # Number of activities (activity_count extra)
             new_extras['activity_count'] = int(tree.xpath(
                                                'count(/iati-activities/iati-activity)'))
-
 
         # Last updated date (data_updated extra)
         if is_activity_package:
             xpath = 'iati-activity/@last-updated-datetime'
         else:
             xpath = 'iati-organisation/@last-updated-datetime'
-
 
         dates = tree.xpath(xpath) or []
         dates = sorted(dates, reverse=True)
@@ -311,14 +269,12 @@ def archive_package(package_id, context, consecutive_errors=0):
                 log.error('Wrong date format for data_updated for dataset {0}:'
                           ' {1}'.format(package['name'], str(e)))
 
-
         # Check dates
         if last_updated_date:
             fmt = '%Y-%m-%d %H:%M:%S'
             new_extras['data_updated'] = last_updated_date.strftime(fmt)
         else:
             new_extras['data_updated'] = None
-
 
         for key, value in new_extras.iteritems():
             if (value and (not key in extras_dict or
@@ -330,7 +286,6 @@ def archive_package(package_id, context, consecutive_errors=0):
                          '{3}'.format(key, package['name'], old_value, value))
                 extras_dict[unicode(key)] = unicode(value)
 
-
         # At this point, any previous issues should be resolved,
         # delete the issue extras to mark them as resolved
         if 'issue_type' in extras_dict:
@@ -339,15 +294,11 @@ def archive_package(package_id, context, consecutive_errors=0):
                 if key in extras_dict:
                     extras_dict[key] = None
 
-
         if update:
             package['extras'] = extras_to_list(extras_dict)
             return update_package(context, package), None, None
 
-
     return None, None, None
-
-
 
 
 def save_package_issue(context, data_dict, extras_dict, issue_type,
@@ -365,15 +316,10 @@ def save_package_issue(context, data_dict, extras_dict, issue_type,
         extras_dict[u'issue_date'] = (unicode(
                                       datetime.datetime.now().isoformat()))
         data_dict['extras'] = extras_to_list(extras_dict)
-
-
         log.error('Issue found for dataset {0}: {1} - '
                   '{2}'.format(data_dict['name'], issue_type, issue_message))
 
-
         return update_package(context, data_dict), issue_type, issue_message
-
-
 
 
 def update_package(context, data_dict, message=None):
@@ -382,20 +328,12 @@ def update_package(context, data_dict, message=None):
                '{0}'.format(data_dict['name']))
     context['message'] = message
     context['disable_archiver'] = True
-
-
     for extra in data_dict['extras']:
         data_dict[extra['key']] = extra['value']
 
-
     data_dict['extras'] = []
-
-
     updated_package = toolkit.get_action('package_update')(context, data_dict)
     log.debug('Package {0} updated with new extras'.format(data_dict['name']))
-    print "================== Package Updated: {} ===============".format(data_dict['name'])
-
-
     return updated_package
 
 
@@ -433,7 +371,6 @@ def _save_resource(resource, response, max_file_size, chunk_size=1024*16):
 def download(context, resource, url_timeout=URL_TIMEOUT,
              max_content_length=MAX_CONTENT_LENGTH,
              data_formats=DATA_FORMATS):
-
     res = None
     resource_changed = False
 
@@ -442,10 +379,7 @@ def download(context, resource, url_timeout=URL_TIMEOUT,
         'url': resource['url'],
         'url_timeout': url_timeout
     })
-
-
     user_agent_string = config.get('ckanext.archiver.user_agent_string', None)
-
 
     def _download_resource(resource_url, timeout):
         request_headers = {}
@@ -482,23 +416,18 @@ def download(context, resource, url_timeout=URL_TIMEOUT,
         else:
             raise
 
-
     resource_format = resource.get('format', '').lower()
     ct = tasks._clean_content_type(headers.get('content-type', '').lower())
     cl = headers.get('content-length')
-
-
     if resource.get('mimetype') != ct:
         resource_changed = True
         resource['mimetype'] = ct
-
 
     # this is to store the size in case there is an error, but the real size
     # check is done after dowloading the data file, with its real length
     if cl is not None and (resource.get('size') != cl):
         resource_changed = True
         resource['size'] = cl
-
 
     # make sure resource content-length does not exceed our maximum
     if cl and int(cl) >= max_content_length:
@@ -508,14 +437,12 @@ def download(context, resource, url_timeout=URL_TIMEOUT,
         raise tasks.DownloadError("Content-length {0} exceeds maximum allowed"
                                   "value {1}".format(cl, max_content_length))
 
-
     # check that resource is a data file
     if not (resource_format in data_formats or ct.lower().strip('"') in data_formats):
         if resource_changed:
             tasks._update_resource(context, resource, log)
         raise tasks.DownloadError("Of content type {0}, not "
                                   "downloading".format(ct))
-
 
     # get the resource and archive it
     # TODO: remove the Accept-Encoding limitation after upgrading
@@ -529,14 +456,11 @@ def download(context, resource, url_timeout=URL_TIMEOUT,
         res = requests.get(resource['url'], timeout=url_timeout,
                            headers=request_headers, verify=False)
     length, hash, saved_file = _save_resource(resource, res, max_content_length)
-    #length = res.headers.get('Content-Length', 0)
-
 
     # check if resource size changed
     if unicode(length) != resource.get('size'):
         resource_changed = True
         resource['size'] = unicode(length)
-
 
     # check that resource did not exceed maximum size when being saved
     # (content-length header could have been invalid/corrupted, or not accurate
@@ -551,22 +475,18 @@ def download(context, resource, url_timeout=URL_TIMEOUT,
                                   "maximum allowed value of "
                                   "{0}".format(max_content_length))
 
-
     # update the resource metadata in CKAN if the resource has changed
     # IATI: remove generated time tags before calculating the hash
     with open(saved_file, 'r') as f:
         content = f.read()
     content = re.sub(r'generated-datetime="[^"]+"', '', content)
 
-
     resource_hash = hashlib.sha1()
     resource_hash.update(content)
     resource_hash = unicode(resource_hash.hexdigest())
 
-
     if resource.get('hash') != resource_hash:
         resource['hash'] = resource_hash
-
 
     return {'length': length,
             'hash': resource_hash,
