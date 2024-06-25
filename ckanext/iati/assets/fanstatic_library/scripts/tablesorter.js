5c825e3ff7691984075c563657ff80e85daf9c18
cormac hallinan <cormachallinan@derilinx.com>
2024-06-25 11:39:54 +0000

    table header sort default

--- ckanext/iati/assets/fanstatic_library/scripts/tablesorter.js
+++ ckanext/iati/assets/fanstatic_library/scripts/tablesorter.js
@@ -15,14 +15,27 @@ this.ckan.module('table-sorter', function (jQuery, _) {
       var given_order = $("#field-order-by").val();
       console.log(given_order);
       if (given_order == "name asc" || given_order == "title asc"){
-          // Ascending order
           el.tablesorter({sortList: [[0,0]]});
-      }else if (given_order === "name desc" || given_order === "title desc"){
-          // Descending order
-	  el.tablesorter({sortList: [[0,1]]});
-      }else{
-         // This is default order
-         el.tablesorter({sortList: [[0,0]]});
+      } else if (given_order === "name desc" || given_order === "title desc"){
+        el.tablesorter({sortList: [[0,1]]});
+      } else if (given_order === "publisher_iati_id asc"){
+        el.tablesorter({sortList: [[1,0]]});
+      } else if (given_order === "publisher_iati_id desc"){
+        el.tablesorter({sortList: [[1,1]]});
+      } else if (given_order === "publisher_organization_type asc"){
+        el.tablesorter({sortList: [[2,0]]});
+      } else if (given_order === "publisher_organization_type desc"){
+        el.tablesorter({sortList: [[2,1]]});
+      } else if (given_order === "publisher_country asc"){
+        el.tablesorter({sortList: [[3,0]]});
+      } else if (given_order === "publisher_country desc"){
+        el.tablesorter({sortList: [[3,1]]});
+      } else if (given_order === "created asc"){
+        el.tablesorter({sortList: [[5,0]]});
+      } else if (given_order === "created desc"){
+        el.tablesorter({sortList: [[5,1]]});
+      } else{
+        el.tablesorter({sortList: [[0,0]]});
       }}
  }
 });
